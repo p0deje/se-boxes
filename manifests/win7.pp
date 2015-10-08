4aec2870ea53f36c4042247df2d0bb8cf85c40cb
Alex Rodionov <p0deje@gmail.com>
2015-10-08 14:33:59 +0600

    Install ruby2.devkit on Windows 7

--- manifests/win7.pp
+++ manifests/win7.pp
@@ -1,6 +1,12 @@
 package {'ruby':
+  ensure   => '2.1.6',
+  provider => 'chocolatey',
+}
+
+package {'ruby2.devkit':
   ensure   => latest,
   provider => 'chocolatey',
+  require  => Package['ruby'],
 }
 
 package {'python':
@@ -31,4 +37,16 @@ package {'phantomjs':
 package {'google-chrome-x64':
   ensure   => $chrome_version,
   provider => 'chocolatey',
+}
+
+file {'config-rubydevkit':
+  path    => 'C:\tools\DevKit2\config.yml',
+  content => '- C:/tools/ruby21',
+  require => Package['ruby2.devkit'],
+}
+
+exec {'install-rubydevkit':
+  command => 'C:\tools\ruby21\bin\ruby.exe dk.rb install',
+  cwd     => 'C:\tools\DevKit2',
+  require => File['config-rubydevkit'],
 }

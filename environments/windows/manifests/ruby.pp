6b336f878c67ee92c8ae9afa4720e4d5884d0df7
Alex Rodionov <p0deje@gmail.com>
2018-03-01 09:00:21 +0600

    Upgrade Ruby to 2.5

--- environments/windows/manifests/ruby.pp
+++ environments/windows/manifests/ruby.pp
@@ -1,5 +1,5 @@
 package { 'ruby':
-  ensure   => '2.3.3',
+  ensure   => '2.5.0.1',
   provider => 'chocolatey',
 }
 
@@ -11,19 +11,19 @@ package { 'ruby2.devkit':
 
 file { 'config-rubydevkit':
   path    => 'C:/tools/DevKit2/config.yml',
-  content => '- C:/tools/ruby23',
+  content => '- C:/tools/ruby25',
   require => Package['ruby2.devkit'],
 }
 
 exec { 'install-rubydevkit':
   command => 'ruby dk.rb install',
-  path    => 'C:/tools/ruby23/bin',
+  path    => 'C:/tools/ruby25/bin',
   cwd     => 'C:/tools/DevKit2',
   require => File['config-rubydevkit'],
 }
 
 exec { 'install-bundler':
   command => 'gem.cmd install bundler',
-  path    => 'C:/tools/ruby23/bin',
+  path    => 'C:/tools/ruby25/bin',
   require => Package['ruby'],
 }

b0d478cc6dc1c3698199a2a24709b17d40171593
Alex Rodionov <p0deje@gmail.com>
2018-10-11 15:41:01 +0600

    Update Python to 2.7

--- environments/windows/manifests/python.pp
+++ environments/windows/manifests/python.pp
@@ -1,4 +1,10 @@
 package { 'python':
-  ensure   => '2.7.6',
+  ensure   => '2.7.11',
   provider => 'chocolatey',
+}
+
+exec { 'install-requests':
+  command => 'pip.exe install requests',
+  path    => 'C:/tools/python/Scripts',
+  require => Package['python'],
 }

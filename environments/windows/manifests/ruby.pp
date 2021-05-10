59bdd8b6597581035e518d5dc726a6534b652d2e
Alex Rodionov <p0deje@gmail.com>
2021-05-10 10:38:45 -0700

    Upgrade Ruby and Bundler

--- environments/windows/manifests/ruby.pp
+++ environments/windows/manifests/ruby.pp
@@ -1,10 +1,10 @@
 package { 'ruby':
-  ensure   => '2.5.1.2',
+  ensure   => '2.6.5.1',
   provider => 'chocolatey',
 }
 
 exec { 'install-bundler':
-  command => 'gem.cmd install bundler -v 1.17.3',
-  path    => 'C:/tools/ruby25/bin',
+  command => 'gem.cmd install bundler -v 2.1.4',
+  path    => 'C:/tools/ruby26/bin',
   require => Package['ruby'],
 }
